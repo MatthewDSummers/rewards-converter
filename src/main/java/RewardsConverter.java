8f6e17249b30daecbc76fd941fd0fd7a1be6a16b
Matthew Summers <imadethisforgithub@gmail.com>
2024-02-19 16:11:47 +0800

    created RewardValue; edited RewardsConverter

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class RewardsConverter {
     public static void main(String[] args) {
+        // Cash
         var scanner = new Scanner(System.in);
         System.out.println("Welcome to the Credit Card Rewards Converter!");
         System.out.println("Please enter a cash value to convert to airline miles: ");
@@ -16,5 +17,19 @@ public class RewardsConverter {
         System.out.println("converting $" + input_value + " to miles");
         var rewardsValue = new RewardValue(cashValue);
         System.out.println("$" + input_value + " is worth " + rewardsValue.getMilesValue() + " miles");
+
+        // Mileage
+        System.out.println("Please enter a mileage value to convert it to cash: ");
+        var mileage_input = scanner.nextLine();
+        int mileageValue;
+        try{
+            mileageValue = Integer.parseInt(mileage_input);
+        }catch (NumberFormatException exception) {
+            System.out.println("Could not parse mileage value as an integer, exiting");
+            return;
+        }
+        System.out.println("converting " + mileage_input + " miles to cash");
+        var mileageReward = new RewardValue(mileageValue);
+        System.out.println(mileage_input + " mile is worth $" + mileageReward.getCashValue());
     }
 }
